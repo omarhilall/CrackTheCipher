2d73d5e7d92a3c97dc031eaa2f19c8a60b62f2ff
omarhilall <102355844+omarhilall@users.noreply.github.com>
2022-11-30 00:22:57 +0200

    assets

---
+++ Scenes/SceneChange2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange2 : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("Scene4");
+    }
+}
+
